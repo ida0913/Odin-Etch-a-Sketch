b245aed1d094ed2e1c34e711bbc4c4b3218958a1
Adi <adib09132005@gmail.com>
2022-06-07 23:28:04 -0500

    Add colorpicker and clear button

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const container = document.querySelector(".container");
-
-function makeRows(rows, cols) {
+const colorPicker = document.querySelector(".colorpicker");
+let tempCell;
+let currentColor = "rgb(0, 0, 225)";
+colorPicker.oninput = (e) => currentColor = `${e.target.value}`
+function makeGrid(rows, cols) {
     for (let i = 0; i < rows * cols; i++) {
         let newDiv = document.createElement("div");
         newDiv.classList.add("cell");
@@ -10,24 +13,49 @@ function makeRows(rows, cols) {
     }
     container.style.gridTemplateColumns = `repeat(${cols}, auto)`;
     container.style.gridTemplateRows = `repeat(${rows}, auto)`;
+
+    const gridCells = document.querySelectorAll(".cell");
+    gridCells.forEach((gridCell) => {
+        gridCell.addEventListener("mouseover", (e) => {
+            if (e.buttons == 1 || e.buttons == 3) {
+                tempCell = gridCell;
+                changeBackgroundHover(currentColor);
+            }
+        })
+    });
+
+
 };
 
-makeRows(64, 64);
+makeGrid(16, 16)
 
-let tempCell;
-const gridCells = document.querySelectorAll(".cell");
-gridCells.forEach((gridCell) => {
-    gridCell.addEventListener("mouseover", (e) => {
-        if (e.buttons == 1 || e.buttons == 3) {
+const clearButton = document.querySelector(".button");
+clearButton.addEventListener("click", () => clearBackground());
 
-            tempCell = gridCell;
-            changeBackgroundHover("red");
-        }
-    })
-});
+const slider = document.getElementById("myRange");
+const output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function () {
+    output.innerHTML = `${this.value}x${this.value}`;
+    updateGrid();
+}
+
+function clearBackground() {
+
+    const gridCells = document.querySelectorAll(".cell");
+    gridCells.forEach((gridCell) => {
+        gridCell.style.backgroundColor = "white";
+    });
+}
 
 function changeBackgroundHover(color) {
 
     tempCell.style.backgroundColor = color.toLowerCase();
 
+}
+
+function updateGrid() {
+    container.innerHTML = "";
+    makeGrid(slider.value, slider.value);
 }
