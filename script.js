bfbd1d7209589e94fe086f579497df2c3c1b7c2a
Adi <adib09132005@gmail.com>
2022-06-07 22:17:52 -0500

    finished up grid & hover methods

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-let container = document.querySelector(".container");
-let rows = 16, cols = 16;
+const container = document.querySelector(".container");
 
-function makeGrid(rows, cols){
-
-for (let i = 0; i < (rows * cols); i++) {
-
-    const newDiv = document.createElement("div");
-    container.appendChild(newDiv).className = "cell";
-
+function makeRows(rows, cols) {
+    for (let i = 0; i < rows * cols; i++) {
+        let newDiv = document.createElement("div");
+        newDiv.classList.add("cell");
+        // newDiv.innerHTML = `${i+1}`;
+        // newDiv.style.fontSize = "8px";
+        container.appendChild(newDiv);
+    }
+    container.style.gridTemplateColumns = `repeat(${cols}, auto)`;
+    container.style.gridTemplateRows = `repeat(${rows}, auto)`;
 };
 
-};
+makeRows(64, 64);
 
+let tempCell;
+const gridCells = document.querySelectorAll(".cell");
+gridCells.forEach((gridCell) => {
+    gridCell.addEventListener("mouseover", (e) => {
+        if (e.buttons == 1 || e.buttons == 3) {
 
-makeGrid(64, 64)
+            tempCell = gridCell;
+            changeBackgroundHover("red");
+        }
+    })
+});
+
+function changeBackgroundHover(color) {
+
+    tempCell.style.backgroundColor = color.toLowerCase();
+
+}
