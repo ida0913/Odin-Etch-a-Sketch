0a38ec394962cac35c7d24cc651d72dde34c9d44
Adi <adib09132005@gmail.com>
2022-06-11 13:22:22 -0500

    Add finishing touches

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+//Initilize divs
 const container = document.querySelector(".container");
 const colorPicker = document.querySelector(".colorpicker");
+//initilize cell and default color
 let tempCell;
 let currentColor = "rgb(0, 0, 225)";
-colorPicker.oninput = (e) => currentColor = `${e.target.value}`
+colorPicker.oninput = (e) => currentColor = `${e.target.value}`;
+
+//create grid function
 function makeGrid(rows, cols) {
     for (let i = 0; i < rows * cols; i++) {
         let newDiv = document.createElement("div");
@@ -26,35 +30,58 @@ function makeGrid(rows, cols) {
 
 
 };
-
+//init  default 16x16 grid
 makeGrid(16, 16)
 
+//intilize buttons
 const clearButton = document.querySelector(".button");
 clearButton.addEventListener("click", () => clearBackground());
 
+const rainbowButton = document.querySelector(".rainbow");
+rainbowButton.addEventListener("click", () => currentColor = "rainbow");
+
 const slider = document.getElementById("myRange");
 const output = document.getElementById("demo");
-output.innerHTML = slider.value;
 
+
+//initlize default output for slider
+output.innerHTML = `${slider.value}x${slider.value}`;
+
+//update grid with slider value
 slider.oninput = function () {
     output.innerHTML = `${this.value}x${this.value}`;
     updateGrid();
 }
 
+//clears background of each grid cell without deleting it
 function clearBackground() {
 
     const gridCells = document.querySelectorAll(".cell");
     gridCells.forEach((gridCell) => {
-        gridCell.style.backgroundColor = "white";
+        gridCell.style.backgroundColor = "transparent";
     });
 }
 
+
+//changes background color when hover and clicked on grid cell
+let i = 0;
 function changeBackgroundHover(color) {
+    if (color !== "rainbow") {
+        tempCell.style.backgroundColor = color.toLowerCase();
+    } else {
+        const rainbowColors = ["violet", "indigo", "blue", "green", "yellow", "orange", "red"];
 
-    tempCell.style.backgroundColor = color.toLowerCase();
+        // let randomRainbowColor = Math.floor(Math.random() * 8) + 1;
+        tempCell.style.backgroundColor = rainbowColors[i];
 
+        // tempCell.style.backgroundColor = rainbowColors[randomRainbowColor];
+        i++;
+        if (i > 6) i = 0;
+    }
 }
 
+
+//helper function to update grid
 function updateGrid() {
     container.innerHTML = "";
     makeGrid(slider.value, slider.value);
